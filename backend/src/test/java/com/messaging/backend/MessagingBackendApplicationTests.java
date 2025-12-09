b66e79161aa064c8bb3f169b2244ca217a35c22f
Abdoulahi Diallo <abdoulahi@Abdoulahis-MacBook-Pro.local>
2025-12-09 17:14:39 +0000

    Replaced JHipster with just Spring Boot, had errors with JHipster that coudn't be easily resolved

---
+++ backend/src/test/java/com/messaging/backend/MessagingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messaging.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
